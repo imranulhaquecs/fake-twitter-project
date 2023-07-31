0cf9f1ecb4aabc506859fe30d21f2e05c4f91ccb
imranulhaquecs <imranul.haque.cs@gmail.com>
2023-07-31 17:01:46 +0600

    signup, signin, user and home pages are created

--- fake-twitter/src/app/app.module.ts
+++ fake-twitter/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { UserComponent } from './components/user/user.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    UserComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
